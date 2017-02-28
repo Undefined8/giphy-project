b6596447af6aa607325fd76bc8b66f22cffd971c
undefined8 <dzamilatskiy2019@bklawtech.com>
2017-02-28 22:55:24 +0000

    featurz

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ var searchTerm;
 
 $("#search").click(function(){
     searchTerm = $("input").val();
-    console.log(encodeURIComponent(searchTerm.toLowerCase()));
-    $.getJSON("https://api.giphy.com/v1/gifs/search?q="+searchTerm.toLowerCase()+"&api_key=dc6zaTOxFJmzC",
+    $.getJSON("https://api.giphy.com/v1/gifs/search?q="+encodeURIComponent(searchTerm.toLowerCase())+"&api_key=dc6zaTOxFJmzC",
     function(response){
-        console.log(response);
+        $("#results").html("");
+        for(var i in response.data)
+            $("#results").append("<img src='"+response.data[i].images.original.url+"'/>");
     });
 });
-
+$("#input").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#search").click();
+    }/*else if(event.keyCode >= 49 && event.keyCode <= 90){
+        $("#search").click();
+    }*/
+});
+$("#reset").click(function(){
+   $("input").val("");
+   $("#results").html("");
+});
